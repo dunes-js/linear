fab39b6f905269efc91652cec73a65a153c3c057
Omar <omarfkuri@gmail.com>
2025-06-03 17:59:50 -0600

    Add radians and deg functions

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
 
 export * from "./mat/index.js"
 export * from "./vec/index.js"
+
+export function radians(degrees: number): number
+{
+	return degrees * (Math.PI / 180.0);
+}
+
+export function degrees(radians: number): number
+{
+	return radians * (180.0 / Math.PI);
+}
