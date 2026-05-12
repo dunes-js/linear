2f6a99369231a6412c4d07a5c83a1d9f33901721
Omar <omarfkuri@gmail.com>
2026-05-11 20:37:14 -0600

    Make matrices column major

--- src/mat/index.ts
+++ src/mat/index.ts
@@ -14,83 +14,98 @@ class Mat<Dim extends number>
 	constructor(public readonly dim: Dim, init: number = 0)
 	{
 		this.array = Array.from({ length: dim }, () =>
-			Array.from({ length: dim }, () => init) as VecData<Dim>
+			Array.from({ length: dim }, () => init)
 		) as MatData<Dim>;
 	}
 
 	public null(): this
 	{
-		const ctor = this.constructor as new (arg: any) => this;
+		const ctor = this.constructor as new(arg: any) => this;
 		return new ctor(0);
 	}
 
 	public identity(): this
 	{
-		for (let i = 0; i < this.dim; i++)
+		for (let col = 0; col < this.dim; col++)
+		{
+			for (let row = 0; row < this.dim; row++)
 			{
-			for (let j = 0; j < this.dim; j++)
-				{
-				this.array[i]![j] = i === j ? 1 : 0;
+				this.array[col]![row] = col === row ? 1 : 0;
 			}
 		}
+
 		return this;
 	}
 
 	public *[Symbol.iterator](): Iterator<number>
 	{
-		for (let i = 0; i < this.dim; i++)
+		for (let col = 0; col < this.dim; col++)
+		{
+			for (let row = 0; row < this.dim; row++)
 			{
-			for (let j = 0; j < this.dim; j++)
-				{
-				yield this.array[i]![j]!;
+				yield this.array[col]![row]!;
 			}
 		}
 	}
 
 	public data(): MatData<Dim>
 	{
-		const copy = this.array.map(row => ([...row] as VecData<Dim>));
-		return copy as MatData<Dim>;
+		return this.array.map(
+			col => [...col] as VecData<Dim>
+		) as MatData<Dim>;
 	}
 
-	public get(row: number, col: number): number {
-		return this.array[row]![col]!;
+	public get(row: number, col: number): number
+	{
+		return this.array[col]![row]!;
 	}
 
 	public setAt(row: number, col: number, value: number): this
 	{
-		this.array[row]![col] = value;
+		this.array[col]![row] = value;
 		return this;
 	}
 
 	public set(mat: MatData<Dim>): this
 	{
-		for (let i = 0; i < this.dim; i++)
+		for (let col = 0; col < this.dim; col++)
+		{
+			for (let row = 0; row < this.dim; row++)
 			{
-			for (let j = 0; j < this.dim; j++)
-				{
-				this.array[i]![j] = mat[i]![j]!;
+				this.array[col]![row] = mat[col]![row]!;
 			}
 		}
+
 		return this;
 	}
 
 	public multiply(other: Mat<Dim>): this
 	{
 		const result = this.null();
-		for (let i = 0; i < this.dim; i++)
+
+		for (let col = 0; col < this.dim; col++)
+		{
+			for (let row = 0; row < this.dim; row++)
 			{
-			for (let j = 0; j < this.dim; j++)
-				{
 				let sum = 0;
+
 				for (let k = 0; k < this.dim; k++)
-					{
-					sum += this.array[i]![k]! * other.array[k]![j]!;
+				{
+					sum +=
+						this.array[k]![row]! *
+						other.array[col]![k]!;
 				}
-				result.array[i]![j] = sum;
+
+				result.array[col]![row] = sum;
 			}
 		}
+
 		return result;
+	}
+
+	public toFloat32Array(): Float32Array
+	{
+		return new Float32Array([...this]);
 	}
 }
 
@@ -98,20 +113,25 @@ export class Mat4 extends Mat<4>
 {
 	constructor(init: number);
 	constructor(mat: Mat4Data);
+
 	constructor(arg: number | Mat4Data)
 	{
 		if (typeof arg === "number")
 		{
 			super(4, arg);
-		} else {
+		}
+		else
+		{
 			super(4, 0);
 			this.set(arg);
 		}
 	}
 
-	public static translation(position: Vec3): Mat4 {
+	public static translation(position: Vec3): Mat4
+	{
 		const [x, y, z] = position;
-		return new Mat4(0).set([
+
+		return new Mat4([
 			[1, 0, 0, 0],
 			[0, 1, 0, 0],
 			[0, 0, 1, 0],
@@ -119,38 +139,50 @@ export class Mat4 extends Mat<4>
 		]);
 	}
 
-	public static rotation(angle: number, axis: Vec3): Mat4 {
+	public static rotation(angle: number, axis: Vec3): Mat4
+	{
 		const [x, y, z] = axis;
-		const len = Math.hypot(x!, y!, z!);
-		if (len === 0) throw new Error("Zero-length axis");
 
-		const nx = x! / len,
-			ny = y! / len,
-			nz = z! / len;
-		const c = Math.cos(angle),
-			s = Math.sin(angle),
-			t = 1 - c;
+		const len = Math.hypot(x!, y!, z!);
+
+		if (len === 0)
+		{
+			throw new Error("Zero-length axis");
+		}
+
+		const nx = x! / len;
+		const ny = y! / len;
+		const nz = z! / len;
+
+		const c = Math.cos(angle);
+		const s = Math.sin(angle);
+		const t = 1 - c;
 
 		return new Mat4([
 			[
 				t * nx * nx + c,
-				t * nx * ny + s * nz,
-				t * nx * nz - s * ny,
-				0,
-			],
-			[
 				t * nx * ny - s * nz,
-				t * ny * ny + c,
-				t * ny * nz + s * nx,
-				0,
+				t * nx * nz + s * ny,
+				0
 			],
 			[
-				t * nx * nz + s * ny,
+				t * nx * ny + s * nz,
+				t * ny * ny + c,
 				t * ny * nz - s * nx,
-				t * nz * nz + c,
-				0,
+				0
 			],
-			[0, 0, 0, 1],
+			[
+				t * nx * nz - s * ny,
+				t * ny * nz + s * nx,
+				t * nz * nz + c,
+				0
+			],
+			[
+				0,
+				0,
+				0,
+				1
+			]
 		]);
 	}
 
@@ -168,10 +200,30 @@ export class Mat4 extends Mat<4>
 		const nf = 1 / (far - near);
 
 		return new Mat4([
-			[2 * lr, 0, 0, -(right + left) * lr],
-			[0, 2 * bt, 0, -(top + bottom) * bt],
-			[0, 0, -2 * nf, -(far + near) * nf],
-			[0, 0, 0, 1]
+			[
+				2 * lr,
+				0,
+				0,
+				0
+			],
+			[
+				0,
+				2 * bt,
+				0,
+				0
+			],
+			[
+				0,
+				0,
+				-2 * nf,
+				0
+			],
+			[
+				-(right + left) * lr,
+				-(top + bottom) * bt,
+				-(far + near) * nf,
+				1
+			]
 		]);
 	}
 
@@ -183,14 +235,36 @@ export class Mat4 extends Mat<4>
 	): Mat4
 	{
 		const fovRad = (fovDeg * Math.PI) / 180;
-		const f = 1.0 / Math.tan(fovRad / 2);
+
+		const f = 1 / Math.tan(fovRad / 2);
+
 		const nf = 1 / (near - far);
 
-		return new Mat4(0).set([
-			[f / aspect, 0, 0, 0],
-			[0, f, 0, 0],
-			[0, 0, (far + near) * nf, -1],
-			[0, 0, (2 * far * near) * nf, 0]
+		return new Mat4([
+			[
+				f / aspect,
+				0,
+				0,
+				0
+			],
+			[
+				0,
+				f,
+				0,
+				0
+			],
+			[
+				0,
+				0,
+				(far + near) * nf,
+				-1
+			],
+			[
+				0,
+				0,
+				2 * far * near * nf,
+				0
+			]
 		]);
 	}
 }
